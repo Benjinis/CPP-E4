31b00b1710f0e91c1b8de50d2ee352fa8dbfba76
Benjamin Niscoise <106484024+Benjinis@users.noreply.github.com>
2022-10-10 16:14:03 +0200

    modif

--- is_prime_prog/isPrime.cpp
+++ is_prime_prog/isPrime.cpp
@@ -8,6 +8,10 @@
 //Exercice fait avec Goulven Fournier
 bool isPrime(const int n)
 {
+    if( n == 1)
+    {
+        return false;
+    }
     for (int i = 2 ; i <= sqrt(n); i++)
     {
         if (n % i == 0)
